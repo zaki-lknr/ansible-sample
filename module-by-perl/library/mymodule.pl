e2990ddd733035a460432cd413cba6e780b9d24e
zaki-lknr <zaki.hmkc+github@gmail.com>
2020-09-23 00:00:29 +0900

    add: リスト形式パラメタのモジュール内取り出し

--- module-by-perl/library/mymodule.pl
+++ module-by-perl/library/mymodule.pl
@@ -10,8 +10,11 @@ while (<ARGV>) {
 # param=foo state=present param2=''hoge hoge'' _ansible_check_mode=False _ansible_no_log=False _ansible_debug=False _ansible_diff=False _ansible_verbosity=1 _ansible_version=2.9.10 _ansible_module_name=mymodule _ansible_syslog_facility=LOG_USER _ansible_selinux_special_fs=''[''"''"''fuse''"''"'', ''"''"''nfs''"''"'', ''"''"''vboxsf''"''"'', ''"''"''ramfs''"''"'', ''"''"''9p''"''"'', ''"''"''vfat''"''"'']'' _ansible_string_conversion_action=warn _ansible_socket=None _ansible_shell_executable=/bin/sh _ansible_keep_remote_files=False _ansible_tmpdir=/home/zaki/.ansible/tmp/ansible-tmp-1600577463.6285203-6577-73636997937191/ _ansible_remote_tmp=''~/.ansible/tmp'' 
 
 my $param = getval("param", $param_lines);
+my $values = getval("values", $param_lines);
 
-print qq#{"changed": false, "result": "🐰", "param": "$param"}#;
+my $values_str = join(",", map { '"'.$_.'"' } @$values); # string arrayのreferenceを、カンマ区切り・ダブルクォート囲みの文字列変換
+
+print qq#{"changed": false, "result": "🐰", "param": "$param", "values": [ $values_str ]}#;
 exit 0;
 
 sub getval {
@@ -22,6 +25,20 @@ sub getval {
     if ($data =~ /$key=([^'\s]+)/) {
         # クォートがない単純なやつ
         return $1;
+    }
+    else {
+        if ($data =~ /$key='\[(.*?)\]'/) {
+            # list
+            my $item = [map { s#'"'"'##g; $_ } split /, /, $1];
+            return $item;
+
+            #   values:
+            #     - item1
+            #     - item2
+            #     - item3
+            # は、↓になる(連続シングルクォートは実際には1個)
+            # values=''[''"''"''item1''"''"'', ''"''"''item2''"''"'', ''"''"''item3''"''"'']''
+        }
     }
 
     return "";
